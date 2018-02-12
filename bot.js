b849bb5e3042af7e58cc05b9d809bf80327e0ee9
WatchersGrim <34408960+WatchersGrim@users.noreply.github.com>
2018-02-12 11:48:06 -0500

    Add files via upload

--- bot.js
+++ bot.js
@@ -30,13 +30,6 @@ fs.readdir("./cmds/", (err, files) => {
 bot.on("ready", async () => {
 	console.log(`Bot is ready! ${bot.user.username}`);
 
-	try {
-		let link = await bot.generateInvite(["ADMINISTRATOR"]);
-		console.log(link);
-	} catch(e) {
-		console.log(e.stack);
-	}
-});
 
 bot.on("message", async message => {
 	if(message.author.bot) return;
